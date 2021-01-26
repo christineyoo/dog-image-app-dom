c3b39522b2f610cfb05bfbdcdd63fdf3c39b3165
Christine Yoo <christinecho.s@gmail.com>
2021-01-26 15:40:48 -0800

    added DOM functionality

---
+++ index.js
@@ -0,0 +1,34 @@
+'use strict';
+
+function getDogImage() {
+const inputValue = $("#quantity").val();
+for (let i = 1; i <= inputValue; i++) {
+    fetch('https://dog.ceo/api/breeds/image/random')
+      .then(response => response.json())
+      .then(responseJson => 
+        displayResults(responseJson))
+      .catch(error => alert('Something went wrong. Try again later.'));
+    }
+}
+
+function displayResults(responseJson) {
+  console.log(responseJson);
+  //replace the existing image with the new one
+  $('.results').append(
+    `<img src="${responseJson.message}" class="results-img">`
+  )
+  //display the results section
+  $('.results').removeClass('hidden');
+}
+
+function watchForm() {
+  $('form').submit(event => {
+    event.preventDefault();
+    getDogImage();
+  });
+}
+
+$(function() {
+  console.log('App loaded! Waiting for submit!');
+  watchForm();
+});
