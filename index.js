e3e4eea9b47e250e790aa19c6cadbab87785e376
Christine Yoo <christinecho.s@gmail.com>
2021-01-27 15:36:25 -0800

    implemented clearing current results

--- index.js
+++ index.js
@@ -1,34 +1,36 @@
-'use strict';
+"use strict";
 
 function getDogImage() {
-const inputValue = $("#quantity").val();
-for (let i = 1; i <= inputValue; i++) {
-    fetch('https://dog.ceo/api/breeds/image/random')
-      .then(response => response.json())
-      .then(responseJson => 
-        displayResults(responseJson))
-      .catch(error => alert('Something went wrong. Try again later.'));
-    }
+  const inputValue = $("#quantity").val();
+  for (let i = 1; i <= inputValue; i++) {
+    fetch("https://dog.ceo/api/breeds/image/random")
+      .then((response) => response.json())
+      .then((responseJson) => displayResults(responseJson))
+      .catch((error) => alert("Something went wrong. Try again later."));
+  }
 }
 
 function displayResults(responseJson) {
   console.log(responseJson);
+  //display the results section
+  $(".results").removeClass("hidden");
+
   //replace the existing image with the new one
   $(".results").append(
     `<img src="${responseJson.message}" class="results-img">`
-  )
-  //display the results section
-  $(".results").removeClass('hidden');
+  );
 }
 
 function watchForm() {
-  $("form").submit(event => {
+  $("form").submit((event) => {
     event.preventDefault();
+    // remove the current images
+    $(".results-img").detach();
     getDogImage();
   });
 }
 
-$(function() {
-  console.log('App loaded! Waiting for submit!');
+$(function () {
+  console.log("App loaded! Waiting for submit!");
   watchForm();
 });
